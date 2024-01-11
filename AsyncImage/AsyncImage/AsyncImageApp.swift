615474f8a07515b17a783d84675596c6fe059d9c
Andhika Agung Pratama <79739062+dioptriman@users.noreply.github.com>
2024-01-11 23:31:44 +0700

    Add files via upload

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Andhika Agung Pratama on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
